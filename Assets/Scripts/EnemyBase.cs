4e09e7047218fc8b3c0866162b9089a621a10b89
ninomiyahayato <a7pseej9@icloud.com>
2023-08-17 22:32:04 +0900

    使用scripts

---
+++ Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    [SerializeField, Header("HP")] protected int _enemyHp;
+    [SerializeField, Header("çUåÇóÕ")] protected int _enemyAttackPower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField, Header("ˆÚ“®‘¬“x")] int _moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
