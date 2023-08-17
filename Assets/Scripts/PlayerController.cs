d2469fd1b2cc28ed4aa53b9fbee79a3bdb443f38
ninomiyahayato <a7pseej9@icloud.com>
2023-08-17 23:05:05 +0900

    playerの基本の動き

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float _v; //vertical
+    float _h; //holizontal
+    Rigidbody _rb;
+    Vector3 _dir;
     [SerializeField, Header("ˆÚ“®‘¬“x")] int _moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _v = Input.GetAxisRaw("Vertical");
+        _h = Input.GetAxisRaw("Horizontal");
     }
     private void FixedUpdate()
     {
-        
+        _dir = Vector3.forward * _h + Vector3.up * _v;
+        _rb.velocity = _dir.normalized * _moveSpeed;
     }
 }
