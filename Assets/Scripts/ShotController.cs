fe8487ab49598e4f8dc5dc9848c151debc144cf7
ninomiyahayato <a7pseej9@icloud.com>
2023-08-29 17:50:26 +0900

    機体に応じてrayを出すように修正

--- Assets/Scripts/ShotController.cs
+++ Assets/Scripts/ShotController.cs
@@ -5,34 +5,27 @@ using UnityEngine.UI;
 
 public class ShotController : MonoBehaviour
 {
-    [SerializeField, Header("照準のImage")] Image _crosshair;
     [SerializeField, Header("射程距離")] float _range = 10;
     [SerializeField, Header("射撃場所")] Transform _shotPosition;
     [SerializeField, Header("対象")] LayerMask _layerMask;
     [SerializeField] LineRenderer _lineRenderer;
+    [SerializeField, Header("照準")] Image _crossHair;
 
-    // Update is called once per frame
     void Update()
     {
-        // マウスカーソルの位置をワールド座標に変換
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z = _range; // 射程距離に設定
-        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        Ray ray = new Ray(_shotPosition.position, targetPosition - _shotPosition.position);
+        Ray ray = new Ray(_shotPosition.position, -_shotPosition.right);
+
         Debug.DrawRay(ray.origin, ray.direction * _range, Color.red);
-        RaycastHit hit = default;
-        Vector3 hitPosition = _shotPosition.position + ray.direction * _range;
-        Collider hitCollider = default;
 
-        if (Physics.Raycast(ray, out hit, _range))
+        RaycastHit hit = default;
+        Vector3 hitPosition = _shotPosition.transform.position + _shotPosition.forward * _range;
+        Collider hitCollider = default;
+        if (Physics.Raycast(ray, out hit, _range, _layerMask))
         {
             hitPosition = hit.point;
             hitCollider = hit.collider;
         }
-
-        // Crosshairの位置をマウスのワールド座標に合わせる
-        _crosshair.rectTransform.position = Camera.main.WorldToScreenPoint(targetPosition);
 
         if (Input.GetButtonDown("Fire1"))
         {
