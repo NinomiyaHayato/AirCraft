8ffff3d6d2ceba6faf868f8627da0ea7386749d9
ninomiyahayato <a7pseej9@icloud.com>
2023-09-09 23:45:51 +0900

    少し作成

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class BossController : EnemyBase
 {
+    Rigidbody _rb;
+    PlayerController _playerController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _playerController = FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/EnemyBase.cs
+++ Assets/Scripts/EnemyBase.cs
@@ -6,15 +6,24 @@ public class EnemyBase : MonoBehaviour
 {
     [SerializeField, Header("HP")] protected int _enemyHp;
     [SerializeField, Header("UŒ‚—Í")] protected int _enemyAttackPower;
+    [SerializeField, Header("ˆÚ“®‘¬“x")] int _enemyMoveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void EnemyDamage(int damage)
+    {
+        _enemyHp -= damage;
+        if(_enemyHp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
