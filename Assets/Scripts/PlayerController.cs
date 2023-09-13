555089212149fb525f57a758d3e38284945051cd
ninomiyahayato <a7pseej9@icloud.com>
2023-09-13 22:02:22 +0900

    ゲームの見直し

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,39 +23,12 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        _dir = Vector3.forward * _h + Vector3.up * _v;
-        _rb.velocity = _dir.normalized * _moveSpeed;
-        VerticalRotation();
-        HolizontalRoration();
-    }
-    private void VerticalRotation()@//player‚ÌŠp“x’²®
-    {
-        if (_h > 0)
+        _dir = Vector3.forward * _h + Vector3.right * _v;
+        _dir = Camera.main.transform.TransformDirection(_dir);
+        if(_v != 0 || _h != 0)
         {
-            this.transform.rotation = _quaternionFirst * Quaternion.Euler(25f, 0f, 0f);
+            transform.forward = _dir;
         }
-        else if (_h < 0)
-        {
-            this.transform.rotation = _quaternionFirst * Quaternion.Euler(-25f, 0f, 0f);
-        }
-        else if(_h == 0 && _v == 0)
-        {
-            this.transform.rotation = _quaternionFirst;
-        }
-    }
-    private void HolizontalRoration() //player‚ÌŠp“x’²®
-    {
-        if (_v > 0)
-        {
-            this.transform.rotation = _quaternionFirst * Quaternion.Euler(0f, 0f, -10f);
-        }
-        else if (_v < 0)
-        {
-            this.transform.rotation = _quaternionFirst * Quaternion.Euler(0f, 0f, 10f);
-        }
-        else if(_v == 0 && _h == 0)
-        {
-            this.transform.rotation = _quaternionFirst;
-        }
+        _rb.velocity = _dir.normalized * _moveSpeed + _rb.velocity.y * Vector3.up;
     }
 }
